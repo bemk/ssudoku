8d4c9f08502d7a2429539bd9b063a7c494a883da
Bart Kuivenhoven <bemkuivenhoven@gmail.com>
2023-03-02 00:43:00 +0100

    Load up an actual sudoku game

--- src/main.cpp
+++ src/main.cpp
@@ -70,15 +70,31 @@ int main(int argc, char** argv)
 	}
 
 	std::vector<std::string> script;
-	script.push_back("a1 = 9");
+	script.push_back("c1 = 3");
+	script.push_back("b1 = 9");
 	script.push_back("b2 = 8");
-	script.push_back("c3 = 7");
-	script.push_back("d4 = 6");
-	script.push_back("e5 = 5");
-	script.push_back("f6 = 4");
-	script.push_back("g7 = 3");
-	script.push_back("h8 = 2");
-	script.push_back("i9 = 1");
+	script.push_back("a3 = 7");
+	script.push_back("e2 = 5");
+	script.push_back("d3 = 8");
+	script.push_back("i1 = 2");
+	script.push_back("h3 = 4");
+	script.push_back("i3 = 9");
+	script.push_back("c5 = 6");
+	script.push_back("a6 = 9");
+	script.push_back("d6 = 5");
+	script.push_back("f5 = 3");
+	script.push_back("g4 = 1");
+	script.push_back("h6 = 7");
+	script.push_back("i6 = 8");
+	script.push_back("c7 = 9");
+	script.push_back("a9 = 1");
+	script.push_back("e7 = 6");
+	script.push_back("d8 = 4");
+	script.push_back("h7 = 1");
+	script.push_back("i7 = 4");
+	script.push_back("g8 = 2");
+	script.push_back("g9 = 5");
+
 	if (verbose) {
 		script.push_back("run verbose");
 	} else {
